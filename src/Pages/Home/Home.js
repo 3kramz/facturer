2e60c19de14ada8ddb6e62b63f44331d96cacb9f
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-05-23 00:06:47 +0600

    Home page reviews

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,13 +3,17 @@ import Banner from './Banner/Banner'
 import Footer from '../../Components/Footer/Footer'
 import Parts from './Parts/Parts';
 import Review from './Review/Review';
+import BusinessSummary from './BusinessSummary/BusinessSummary';
+
+
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Parts />
-            <Review />
+            <Review/>
+            <BusinessSummary />
             <Footer />
         </div>
     );
