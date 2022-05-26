4ec221d44e1dd44f4d4c8c0ab8307290de8c9b8a
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-05-26 22:15:07 +0600

    My portFolio

--- src/Pages/MyPortfolio/MyPortFfolio.js
+++ src/Pages/MyPortfolio/MyPortFfolio.js
@@ -2,8 +2,61 @@ import React from 'react';
 
 const MyPortFfolio = () => {
     return (
-        <div>
-            <p className="text-5xl">My PortFolio</p>
+        <div className="p-10">
+            <div class="card p-5 bg-base-100 shadow-xl">
+                <div class="avatar card-body items-center text-center">
+                    <div class="w-60  rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src="https://i.ibb.co/B6yF332/msg1376083639-323.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title text-3xl">Sultan Mahmud Ekram</h2>
+                    <p> I have just over a year of experience as a junior software engineer with First Technology. In my short time there, I've already contributed to over a dozen projects and assisted with managing one project for one of the firm's long-time clients</p>
+                    <table class="table w-full mx-auto">
+                        <tbody>
+
+                            <tr>
+                                <th>Email</th>
+                                <td><p>smahmud1098@gmail.com</p></td>
+                            </tr>
+                            <tr>
+                                <th>Educaton</th>
+                                <td><p>Computer Science And Engineering, <br />
+                                    Univarsity of Brahmanbaria</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>List of Technologies</th>
+                                <td>
+                                    <ul>
+                                        <li>HTML</li>
+                                        <li>css</li>
+                                        <li>Javascriot</li>
+                                        <li>React JS</li>
+                                        <li>Node JS</li>
+                                        <li>MongoDB</li>
+                                        <li>Bootstrap</li>
+                                        <li>Tailwind css</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Best</th>
+                                <td>
+                                <ul>
+                                        <li><a  href="https://riven-driven.web.app/"> Riven Driven</a></li>
+                                        <li><a  href="https://klub-fit.web.app/">Klub fit</a></li>
+                                        <li><a  href="https://tesla-s-review.netlify.app/">Tesla Car Review </a></li>
+                                        
+                                    </ul>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
         </div>
     );
 };
